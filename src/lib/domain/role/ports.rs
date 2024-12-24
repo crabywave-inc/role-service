58eea0681cbb30d2634e09a09edfc532315ec022
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-24 23:08:45 +0100

    feat: update role_repository trait

--- src/lib/domain/role/ports.rs
+++ src/lib/domain/role/ports.rs
@@ -1,3 +1,10 @@
+use std::future::Future;
+use crate::domain::role::entities::error::RoleError;
+use crate::domain::role::entities::model::Role;
+
 pub trait RoleService: Clone + Send + Sync + 'static {}
 
-pub trait RoleRepository: Clone + Send + Sync + 'static {}
+pub trait RoleRepository: Clone + Send + Sync + 'static {
+    fn create(&self) -> impl Future<Output = Result<Role, RoleError>> + Send;
+    fn find_by_server_id(&self, role_id: &str) -> impl Future<Output=Result<Vec<Role>, RoleError>> + Send;
+}
--- src/lib/infrastructure/role/db/firestore_role_repository.rs
+++ src/lib/infrastructure/role/db/firestore_role_repository.rs
@@ -1,6 +1,9 @@
+use std::future::Future;
 use crate::domain::role::ports::RoleRepository;
 use crate::infrastructure::db::firestore::Firestore;
 use std::sync::Arc;
+use crate::domain::role::entities::error::RoleError;
+use crate::domain::role::entities::model::Role;
 
 #[derive(Debug, Clone)]
 pub struct FirestoreRoleRepository {
@@ -13,4 +16,12 @@ impl FirestoreRoleRepository {
     }
 }
 
-impl RoleRepository for FirestoreRoleRepository {}
+impl RoleRepository for FirestoreRoleRepository {
+    async fn create(&self) -> Result<Role, RoleError>{
+        todo!()
+    }
+
+    async fn find_by_server_id(&self, role_id: &str) -> Result<Vec<Role>, RoleError> {
+        todo!()
+    }
+}
