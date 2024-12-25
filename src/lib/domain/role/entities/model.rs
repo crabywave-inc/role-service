fc13c6a671d7c5a3b0c00a373c481e648ab95c00
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-25 01:03:54 +0100

    feat: update Role models

--- src/lib/domain/role/entities/model.rs
+++ src/lib/domain/role/entities/model.rs
@@ -5,4 +5,18 @@ pub struct Role {
     pub id: String,
     pub name: String,
     pub server_id: String,
+    pub color: String,
+    pub position: i32,
+    pub permissions: String,
+    pub hoist: bool,
+    pub mentionable: bool,
+}
+
+#[derive(Deserialize)]
+pub struct CreateRoleRequest {
+    pub name: String,
+    pub permissions: String,
+    pub color: String,
+    pub hoist: bool,
+    pub mentionable: bool,
 }
