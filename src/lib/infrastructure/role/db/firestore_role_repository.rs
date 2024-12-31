98dc7719241d1a3182c8e629b04c7017238d1002
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-31 17:55:41 +0100

    feat: Refactor FirestoreRoleRepository to use guild_id instead of server_id
    This commit updates the FirestoreRoleRepository to use the `guild_id` field instead of `server_id` for better alignment with the terminology used in the system. This change improves clarity and consistency in the codebase.

--- src/lib/infrastructure/role/db/firestore_role_repository.rs
+++ src/lib/infrastructure/role/db/firestore_role_repository.rs
@@ -1,3 +1,5 @@
+use firestore::FirestoreQueryFilter;
+
 use crate::domain::role::entities::error::RoleError;
 use crate::domain::role::entities::model::{CreateRoleRequest, Role};
 use crate::domain::role::ports::role::RoleRepository;
@@ -16,12 +18,12 @@ impl FirestoreRoleRepository {
 }
 
 impl RoleRepository for FirestoreRoleRepository {
-    async fn create(&self, server_id: &str, payload: CreateRoleRequest) -> Result<Role, RoleError> {
+    async fn create(&self, guild_id: &str, payload: CreateRoleRequest) -> Result<Role, RoleError> {
         let id = uuid::Uuid::new_v4().to_string();
         let role = Role {
             id,
             name: payload.name.to_string(),
-            server_id: server_id.to_string(),
+            guild_id: guild_id.to_string(),
             color: payload.color,
             position: 0,
             permissions: payload.permissions,
@@ -43,8 +45,20 @@ impl RoleRepository for FirestoreRoleRepository {
         Ok(role)
     }
 
-    async fn find_by_server_id(&self, _server_id: &str) -> Result<Vec<Role>, RoleError> {
-        todo!()
+    async fn find_by_guild_id(&self, guild_id: &str) -> Result<Vec<Role>, RoleError> {
+        let roles = self
+            .firestore
+            .db
+            .fluent()
+            .select()
+            .from("roles")
+            .filter(|q| q.for_all([q.field("guild_id").eq(guild_id)]))
+            .obj::<Role>()
+            .query()
+            .await
+            .map_err(|_| RoleError::InternalServerError)?;
+
+        Ok(roles)
     }
 
     async fn find_by_id(&self, role_id: &str) -> Result<Option<Role>, RoleError> {
